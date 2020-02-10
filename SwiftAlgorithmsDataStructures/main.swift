096d56d2b7e19a31cbbc913463975c3b80e549b0
WendaLi <lwd19861127@hotmail.com>
2020-02-10 15:36:07 -0800

    Initial Commit

---
+++ SwiftAlgorithmsDataStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithmsDataStructures
+//
+//  Created by WendaLi on 2020-02-10.
+//  Copyright © 2020 WendaLi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
