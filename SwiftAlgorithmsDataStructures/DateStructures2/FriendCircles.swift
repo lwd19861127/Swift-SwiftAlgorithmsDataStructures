626217607fcd3668c30b8b50dda717481e6a82fe
WendaLi <lwd19861127@hotmail.com>
2020-02-26 16:41:33 -0800

    FriendCircles

--- SwiftAlgorithmsDataStructures/DateStructures2/FriendCircles.swift
+++ SwiftAlgorithmsDataStructures/DateStructures2/FriendCircles.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+func findCircleNum(_ M: [[Int]]) -> Int {
+    
+    //矩阵是对称的 矩阵斜线上方三角形的边长
+    let n = M.count
+    if n == 1 {return 1}
+    
+    let studentsNum = n
+    var students = UF(studentsNum)
+    
+    for i in 0..<n-1 {
+        for j in i+1...n-1 {
+            if M[i][j] == 1 {
+                students.union(i, j)
+            }
+        }
+    }
+    return students.count
+}
