d9eb0bbbebced6f414a22591d86341e087bc60c1
WendaLi <lwd19861127@hotmail.com>
2020-03-17 13:06:22 -0700

    update

--- SwiftAlgorithmsDataStructures/Graph/TopologicalSort.swift
+++ SwiftAlgorithmsDataStructures/Graph/TopologicalSort.swift
@@ -2,16 +2,6 @@
 //  TopologicalSort.swift
 //  SwiftAlgorithmsDataStructures
 //
-//  Created by WendaLi on 2020-03-05.
-//  Copyright © 2020 WendaLi. All rights reserved.
-//
-
-import Foundation
-
-//
-//  TopologicalSort.swift
-//  SwiftAlgorithmsDataStructures
-//
 //  Created by Derrick Park on 3/3/20.
 //  Copyright © 2020 Derrick Park. All rights reserved.
 //
